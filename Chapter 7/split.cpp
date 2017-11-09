4d07e9bf777e476cc9ca15850c721e751452601f
Stephen King <iStephenKing@me.com>
2017-11-08 23:53:28 -0500

    Played around a bit with reading input from a file rather than std::in

---
+++ Chapter 7/split.cpp
@@ -0,0 +1,45 @@
+/*
+    Split.cpp
+
+    From Accelerated C++ By Andrew Koenig and Barbara E. Moo
+    Implementation of Split functions with iterators
+ 
+    Stephen King
+    8/28/2017
+*/
+
+#include <iostream>
+#include <algorithm>
+
+#include "split.h"
+
+using namespace std;
+
+// Test if character is whitespace
+bool space(char c)
+{
+    return(isspace(c));
+}
+
+// Test if character is not whitespace
+bool not_space(char c)
+{
+    return(!isspace(c));
+}
+
+// Split a string into vector of words
+std::vector<std::string> split(const string& str)
+{
+    typedef string::const_iterator iter;
+    vector<string> ret;
+    
+    iter i = str.begin();
+    while (i != str.end()) {
+        i = find_if(i, str.end(), not_space);
+        iter j = find_if(i, str.end(), space);
+        if (i != str.end())
+            ret.push_back(string(i,j));
+        i = j;
+    }
+    return ret;
+}
