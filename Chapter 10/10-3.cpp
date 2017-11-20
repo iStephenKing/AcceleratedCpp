5759bb024bd11bad8a678ce5f6008687e4211204
Stephen King <iStephenKing@me.com>
2017-11-20 08:49:32 -0500

    Added Test cases for vector<int>, double [] and vector<double>

--- Chapter 10/10-3.cpp
+++ Chapter 10/10-3.cpp
@@ -12,6 +12,7 @@
 */
 
 #include <iostream>
+#include <iomanip>
 #include <string>
 #include <cstring>
 #include <fstream>
@@ -29,6 +30,29 @@ int main(int argc, char** argv)
     
     for (int i = 0; i < length; ++i)
         cout << nums[i] << " ";
+    cout << endl;
+    
+    vector<int> vec(nums, nums+length);
+    cout << "median of vector<int> = " << median<int>(vec.begin(), vec.end()) << endl;
+    
+    for (vector<int>::const_iterator it = vec.begin(); it != vec.end(); ++it)
+        cout << *it << " ";
+    cout << endl;
+    
+    cout << fixed << setprecision(1);
+    double dnums[] = {1.1, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0};
+    size_t dlength = sizeof(dnums) / sizeof(*dnums);
+    cout << "median of double dnums[] = " << median<double>(dnums, dnums + dlength) << endl;
+    
+    for (int i = 0; i < dlength; ++i)
+        cout << dnums[i] << " ";
+    cout << endl;
+    
+    vector<double> dvec(dnums, dnums+dlength);
+    cout << "median of vector<double> = " << median<double>(dvec.begin(), dvec.end()) << endl;
+    
+    for (vector<double>::const_iterator it = dvec.begin(); it != dvec.end(); ++it)
+        cout << *it << " ";
     cout << endl;
     
     
