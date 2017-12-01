5c7bc164d6b9a7cb458425278200e0cc979104f2
Stephen King <iStephenKing@me.com>
2017-11-30 22:20:23 -0500

    Added c_str, data, and relational comparitors

--- Chapter 12/main.cpp
+++ Chapter 12/main.cpp
@@ -10,6 +10,7 @@
 */
 
 #include <iostream>
+#include <cassert>
 #include "Str2.hpp"
 
 using namespace std;
@@ -17,19 +18,32 @@ using namespace std;
 int main()
 {
     Str s1 = "Hello World!";
-    cout << s1 << endl;
-    cout << s1 << endl;
-    cin >> s1;
-    cout << s1 << endl;
     
-    Str s2(5,'s');
-    cout << s2 << endl;
-    s1 = s2;
-    cout << s1;
-    s2 = "changed";
-    cout << "s1 = " << s1 << " | s2 = " << s2 << endl;
-    s2 += " again";
-    cout << s2 << endl;
-    cout << s1 + s2 + "pizza" + s1 << endl;
+    Str s2 = "ABC";
+    
+    Str s3 = "AAA";
+    
+    // These statements should pass
+    assert(s1 == s1);
+    
+    assert(s3 < s2);
+    
+    assert(s2 > s3);
+    
+    assert(s2 >= s3);
+    
+    assert(s3 <= s2);
+    
+    assert(s1 >= s1);
+    
+    assert(s1 <= s1);
+    
+    Str s4 = "Positive Cases Passed";
+    
+    cout <<  s4 << endl;
+    
+    //assert(s1 == s2);
+    
+    //cout << "Negative Case Passed" << endl;
     
 }
