c42d3ebaec13389a923005080f1a0c59959a2049
Stephen King <iStephenKing@me.com>
2017-12-02 15:00:45 -0500

    Added getline() to Str class as friend function

--- Chapter 12/Str2.cpp
+++ Chapter 12/Str2.cpp
@@ -57,9 +57,12 @@ std::istream& getline(std::istream& is, Str& s)
 
 std::ostream& operator<<(std::ostream& os, const Str& s)
 {
+    /*
     for (Str::size_type i = 0; i != s.size(); ++i) {
         os << s[i];
     }
+     */
+    std::ostream_iterator(data, data + length);
     
     return os;
 }
