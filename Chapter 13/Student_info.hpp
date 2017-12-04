6cdad5920e37e77f3031ab6a31fd24fec58a392e
Stephen King <iStephenKing@me.com>
2017-12-03 19:23:03 -0500

    Updated to allow mixture of Student_info and Grad classes

--- Chapter 13/Student_info.hpp
+++ Chapter 13/Student_info.hpp
@@ -19,6 +19,7 @@ public:
     
     Student_info(): midterm(0), final(0) { }
     Student_info(std::istream& in) { read (in); }
+    virtual ~Student_info() { }
     
     virtual std::istream& read(std::istream&);
     virtual double grade() const;
@@ -39,6 +40,7 @@ private:
 };
 
 bool compare(const Student_info& x, const Student_info& y);
+bool compare_ptrs(const Student_info* student1, const Student_info* student2);
 bool compare_grades(const Student_info&, const Student_info&);
 std::vector<Student_info> classify_students(std::vector<Student_info>& students,
                                     bool classifier(const Student_info&));
--- Chapter 13/main.cpp
+++ Chapter 13/main.cpp
@@ -7,43 +7,57 @@
 //
 
 #include "Student_info.hpp"
+#include "Grad.hpp"
 #include <vector>
 #include <string>
 #include <iostream>
 #include <algorithm>
 #include <iomanip>
+#include <fstream>
 
 using namespace std;
 
 int main()
 {
-    // Base Student info Class test
+    ifstream input("students.txt");
     
-    vector<Student_info> students;
-    Student_info record;
+    // Mixed Student info Class and Child Grad class test
+    vector<Student_info*> students;
+    Student_info* record;
+    char identifier;
     string::size_type max_name_length = 0;
     
     // read and store Student data
-    while (record.read(cin)) {
-        max_name_length = max(max_name_length, record.name().size());
+    while (input >> identifier) {
+        // 'U' signifies undergraduate Student -- Base Class
+        if (identifier == 'U')
+            record = new Student_info;
+        else
+            record = new Grad;
+        
+        record->read(input);
+        max_name_length = max(max_name_length, record->name().size());
         students.push_back(record);
     }
     
     // Compare predicate used to alphabetize by name
-    sort(students.begin(), students.end(), compare);
+    sort(students.begin(), students.end(), compare_ptrs);
     
-    for (vector<Student_info>::const_iterator  it = students.begin(); it != students.end(); ++it) {
-        cout << it->name() << string(max_name_length + 1 - it->name().size(), ' ');
+    for (vector<Student_info*>::const_iterator  it = students.begin(); it != students.end(); ++it) {
+        cout << (*it)->name() << string(max_name_length + 1 - (*it)->name().size(), ' ');
         
         try {
-            double final_grade = it->grade();
+            double final_grade = (*it)->grade();
             streamsize prec = cout.precision();
             cout << setprecision(3) << final_grade << setprecision(prec) << endl;
         } catch (domain_error e) {
             cout << e.what() << endl;
         }
+        delete *it;
     }
+
     
+
     return 0;
     
 }
